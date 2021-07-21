9fa7ea11426b9318bd31e400056559771a62cdd4
Nguyen Trieu <nguyentrieussj4@gmail.com>
2021-07-21 15:57:38 +0700

    add chart

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSE = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 10,
+    date: new Date(2021, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "Playtation 5",
+    amount: 499,
+    date: new Date(2020, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2019, 3, 28),
+  },
+  {
+    id: "e4",
+    title: "Valve Steam Desk",
+    amount: 649,
+    date: new Date(2019, 2, 28),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 10,
-      date: new Date(2021, 7, 14),
-    },
-    {
-      id: "e2",
-      title: "Playtation 5",
-      amount: 499,
-      date: new Date(2021, 2, 12),
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "Valve Steam Desk",
-      amount: 649,
-      date: new Date(2021, 12, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSE);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    // Not clean way
+    // setExpenses([expense, ...expenses]);
+    // Clean way
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,20 +4,16 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ title, date, amount }) => {
-  const [titleName, setTitleName] = useState(title);
-
-  const clickHandler = () => {
-    setTitleName("Trieu thong minh");
-  };
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={date} />
-      <div className="expense-item__description">
-        <h2>{titleName}</h2>
-        <div className="expense-item__price">${amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">${amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 
